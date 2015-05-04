8dee921d22cfa13a75f63f89fe962e87e3c85f01
souzabrizolara <souzabrizolara@gmail.com>
2015-05-04 17:26:23 -0300

    Testando controles (switch) em appliances-list.

--- js/MainController.js
+++ js/MainController.js
@@ -1,7 +1,7 @@
 angular.module('homeshell').controller("MainController", function(){
     this.title = 'Title';
     this.console = console;
-    
+    this.document = document;
     this.onLoadFunction = function(){
         console.log("loaded!");
         DashgumSidebar();
