abfce37552553db60733f5159e6d0858351a63c8
souzabrizolara <souzabrizolara@gmail.com>
2015-06-08 11:40:31 -0300

    Iniciando geração de interface. - Incluindo exemplos de esquema da interface.

--- js/app.js
+++ js/app.js
@@ -53,7 +53,15 @@ app.controller('IndexAppliancesController', function($scope, $http) {
     $scope.appliances = [];
     $scope.loading = true;
     $scope.appliancesById = [];
+    //Lista de schemes
+    $scope.uiSchemes = [];
+    //Appliance q está selecionada
+    $scope.currentAppliance = null;
 
+    $scope.setup = function(){
+        $scope.getItems();
+        $scope.getSchemes();
+    }
 
     $scope.processAppliances = function(){
         var applianceCount = $scope.appliances.length;
@@ -97,6 +105,37 @@ app.controller('IndexAppliancesController', function($scope, $http) {
             alert("Could not retrieve data from server");
         });
     };
+
+
+    $scope.getSchemes = function() {
+        var req = {
+            method: 'GET',
+            // url: 'http://localhost:8080/appliances/',
+            // url: 'http://localhost/academico/hs-samples/sample_appliances_a.json',
+            url: 'samples/sample_ui-schemes.json',
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+            }
+        }
+
+        $http(req)
+        .success(function(data, status) {
+            console.log('GOT schemes');
+            $scope.uiSchemes = data.schemes;
+        })
+        .error(function(data, status) {
+            console.log("Failed to get schemes", status);
+            alert("Could not retrieve scheme data from server");
+        });
+    };
+
+    $scope.getScheme = function(appliance){
+        if(appliance){
+            var schemeName = appliance.package + "." + appliance.type;
+            return $scope.uiSchemes[schemeName];
+        }
+        return null;
+    }
 
     $scope.lampStatus = function(status){
         if(status == 1){
@@ -146,4 +185,61 @@ app.controller('IndexAppliancesController', function($scope, $http) {
             $scope.appliancesById[lampId].viewStatus = (realStatus == 1);
         });
     }
+
+    $scope.jsonStringify = function(object){
+        return JSON.stringify(object);
+    }
+    $scope.setCurrentAppliance = function(applianceId){
+        console.log("show");
+        $scope.currentAppliance = $scope.appliancesById[applianceId];
+        console.log("end show. Appliance id: ", applianceId);
+    }
 });
+
+//exemplo
+app.directive('childDirective', function ($http, $templateCache, $compile, $parse) {
+return {
+    restrict: 'E',
+    scope: [],
+    compile: function (iElement, iAttrs, transclude) {
+        iElement.append('child directive<br />');
+    }
+}
+});
+
+app.directive('applianceDetail', [function ($compile) {
+   return {
+     restrict: 'E',
+    scope: {
+      appliance: '=appliance'
+    },
+    templateUrl : 'templates/appliance-detail-template.html'
+   };
+ }]);
+
+app.directive('applianceControl', [function ($compile) {
+   return {
+     restrict: 'E',
+     //link:function(scope, element, attrs) {
+    //   var tag = '<input bs-switch type="checkbox" switch-size="small" switch-label-width="0"'
+    //     + 'ng-model="appliancesById[appliance.id].viewStatus"'
+    //     + 'ng-change="toggleStatus(appliancesById[appliance.id].viewStatus ? 1 : 0, appliance.id);"'
+    //     + '>'
+    //   element.append(tag);
+    //   $compile(tag)(scope);
+    // }
+    scope: {
+      appliance: '=appliance'
+    },
+    template : function(element, attrs){
+      return '<input bs-switch type="checkbox" ng-model="appliance.viewStatus"/>';
+    }
+    //compile: function (element, attrs) {
+    //    var x = '<input bs-switch ng-model="">';
+    //    element.append(x);
+    //}
+    //, link: function(scope, element, attrs, controllers) {
+    //  console.log("link, scope: ", scope, "\n Element: ", element );
+    //},
+   };
+ }]);
